4342912667692eaf5959d889b6ad4433b0c4bc96
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2026-08-01 06:37:59 +0000

    change runArgs for CI

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,6 @@
 set -e # Bricht das Skript ab, falls ein Befehl fehlschlägt
 
 echo "=== Starte Devcontainer-Setup ==="
-
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 cargo binstall --no-confirm --disable-telemetry bacon cargo-audit cargo-nextest ripgrep
@@ -11,6 +10,7 @@ echo "Installiere lazygit..."
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 LAZYGIT_ARCH=$(uname -m | sed -e 's/aarch64/arm64/')
 
+cd /tmp
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${LAZYGIT_ARCH}.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
